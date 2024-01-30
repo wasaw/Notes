9372d4a56a1e784d1bd77078d092110024e63cca
wasaw <merenkovalexander@gmail.com>
2024-01-30 17:08:38 +0300

    create note module UI

--- Notes/Presentation/Note/View/NoteViewController.swift
+++ Notes/Presentation/Note/View/NoteViewController.swift
@@ -7,11 +7,35 @@
 
 import UIKit
 
+private enum Constants {
+    static let horizontalPadding: CGFloat = 15
+    static let paddingTop: CGFloat = 12
+    static let titleTextFieldHeight: CGFloat = 55
+    static let noteViewPaddingTop: CGFloat = 25
+}
+
 final class NoteViewController: UIViewController {
     
 // MARK: - Properties
     
     private let output: NoteOutput
+    private lazy var titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Введите заголовок"
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: 15))
+        textField.leftViewMode = .always
+        return textField
+    }()
+    private lazy var noteTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "Содержание"
+        textView.isScrollEnabled = false
+        textView.sizeToFit()
+        textView.font = UIFont.systemFont(ofSize: 21)
+        textView.textColor = .white
+        textView.delegate = self
+        return textView
+    }()
     
 // MARK: - Lifecycle
     
@@ -28,7 +52,31 @@ final class NoteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .orange
+        configureUI()
+    }
+    
+// MARK: - Helpers
+    
+    private func configureUI() {
+        view.addSubview(titleTextField)
+        titleTextField.anchor(leading: view.leadingAnchor, 
+                              top: view.safeAreaLayoutGuide.topAnchor,
+                              trailing: view.trailingAnchor,
+                              paddingLeading: Constants.horizontalPadding,
+                              paddingTop: Constants.paddingTop,
+                              paddingTrailing: -Constants.horizontalPadding,
+                              height: Constants.titleTextFieldHeight)
+        titleTextField.backgroundColor = .tableBackground
+        
+        view.addSubview(noteTextView)
+        noteTextView.anchor(leading: view.leadingAnchor,
+                             top: titleTextField.bottomAnchor,
+                             trailing: view.trailingAnchor,
+                             paddingLeading: Constants.horizontalPadding,
+                             paddingTop: Constants.noteViewPaddingTop,
+                             paddingTrailing: -Constants.horizontalPadding)
+        noteTextView.backgroundColor = .background
+        view.backgroundColor = .background
     }
 }
 
@@ -36,4 +84,20 @@ final class NoteViewController: UIViewController {
 
 extension NoteViewController: NoteInput {
     
+}
+
+// MARK: - UITextView
+
+extension NoteViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == "Содержание" {
+            textView.text = ""
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "Содержание"
+        }
+    }
 }
